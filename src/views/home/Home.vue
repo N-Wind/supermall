e3115ab8f0a33ee73cd4ad2b716b21ef9acef3a5
weatar <huimoulansan@yeah.net>
2020-05-09 08:30:10 +0800

    new folders

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,43 +1,135 @@
 <template>
   <div class="home">
-    <nav-bar class="home-nav">
-        <div slot="center">购物车</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
-  import {getHomeMultidata} from "network/home";
+  import TarBarItem from "components/common/tabbar/TarBarItem";
+
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import {getHomeMultidata, getHomeGoods} from "network/home";
+  import TabControl from "../../components/content/tabControl/TabControl";
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      TabControl,
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView
     },
     data() {
       return {
         // result: null
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'news': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {
       // 1.请求多个数据
       getHomeMultidata().then(res => {
-        console.log(res);
+        // console.log(res);
         // this.result = res;
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
+      })
+
+      // 2.请求商品数据
+      getHomeGoods().then(res => {
+        console.log(res);
       })
     }
   }
 </script>
 
 <style scoped>
+  .home {
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     text-align: center;
     color: #ffffff;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
